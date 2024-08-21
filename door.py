ce6b90c727f827a70d062b23bb5f3bc565f19f06
IM23a-wuk <wuk@bzz.ch>
2024-08-21 15:58:49 +0200

    testing5

--- door.py
+++ door.py
@@ -5,14 +5,13 @@ zur Simulation eines Tür- und Schlossmechanismus.
 
 
 class Door:
-    """
+  """
     Klasse, die eine Tür mit einem Schlossmechanismus simuliert.
 
     :param ref2door_lock: Referenz zu einem Türschloss-Objekt
     :param base_color: Die Farbe der Tür
-    """
-
-    def __init__(self, ref2door_lock, base_color):
+  """
+  def __init__(self, ref2door_lock, base_color):
         """
         Erzeugt ein Tür-Objekt.
         :param ref2door_lock: Referenz zu einem Türschloss-Objekt
@@ -23,45 +22,45 @@ class Door:
         self._door_is_open = False
         self._door_is_locked = False
 
-    def open_the_door(self):
+  def open_the_door(self):
         """Methode für das Öffnen der Tür, wenn sie nicht verriegelt ist."""
         if not self._door_is_locked:
             self._door_is_open = True
 
-    def close_the_door(self):
+  def close_the_door(self):
         """Methode für das Schließen der Tür."""
         self._door_is_open = False
 
-    def lock_the_door(self):
+  def lock_the_door(self):
         """Methode für das Verriegeln der Tür, wenn sie geschlossen ist."""
         if not self._door_is_open:
             self._door_is_locked = self._the_door_lock.lock()
 
-    def unlock_the_door(self):
+  def unlock_the_door(self):
         """Methode für das Entriegeln der Tür, wenn sie verriegelt ist."""
         if self._door_is_locked:
             self._door_is_locked = self._the_door_lock.unlock()
 
-    def test(self):
+  def test(self):
         """Gibt alle Attribute der Tür aus."""
         print(f'Türfarbe: {self.color}\n'
               f'Türe offen: {self._door_is_open}\n'
               f'Türe verriegelt: {self._door_is_locked}')
 
-    @property
-    def door_is_open(self):
+@property
+def door_is_open(self):
         return self._door_is_open
 
-    @property
-    def door_is_locked(self):
+@property
+def door_is_locked(self):
         return self._door_is_locked
 
-    @property
-    def color(self):
+@property
+def color(self):
         return self._color
 
-    @color.setter
-    def color(self, new_color):
+@color.setter
+def color(self, new_color):
         self._color = new_color
 
 
@@ -76,7 +75,6 @@ class DoorLock:
 
     def unlock(self):
         return False
-
 
 if __name__ == "__main__":
     print("Test für Tür-Objekt")
